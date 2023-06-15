7ef904cf92ff064264d9fefb0a7e1e7bcefa51ef
samsiegart <samsiegart@gmail.com>
2023-06-15 04:15:37 -0700

    chore: fix lint

--- packages/web-components/react.js
+++ packages/web-components/react.js
@@ -1,15 +1,9 @@
 import { createComponent } from '@lit-labs/react';
 
-import { AgoricWalletConnection, DappWalletBridge } from './index.js';
+import { DappWalletBridge } from './index.js';
 
 // Upgrade the tags.
-import './agoric-wallet-connection.js';
 import './dapp-wallet-bridge.js';
-
-export const makeReactAgoricWalletConnection = React =>
-  createComponent(React, 'agoric-wallet-connection', AgoricWalletConnection, {
-    onState: 'state',
-  });
 
 export const makeReactDappWalletBridge = React =>
   createComponent(React, 'dapp-wallet-bridge', DappWalletBridge, {
