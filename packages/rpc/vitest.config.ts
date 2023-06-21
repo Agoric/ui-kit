2b1de73979f71dcead77d8dd2d836cdd000c5a61
samsiegart <samsiegart@gmail.com>
2023-06-21 00:54:11 -0700

    feat(rpc): new package with batch query

---
+++ packages/rpc/vitest.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/extensions */
+import { mergeConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'happy-dom',
+    },
+  }),
+);
