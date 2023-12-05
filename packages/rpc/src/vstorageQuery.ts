56246cd7734259ba8a9a32bb884c0722e0c62bee
samsiegart <samsiegart@gmail.com>
2023-12-04 23:51:17 -0800

    feat!: dont batch queries

---
+++ packages/rpc/src/vstorageQuery.ts
@@ -0,0 +1,72 @@
+/* eslint-disable import/extensions */
+import axios from 'axios';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axiosRetry from 'axios-retry';
+import type { FromCapData } from '@endo/marshal';
+import { AgoricChainStoragePathKind } from './types';
+
+export const pathToKey = (path: [AgoricChainStoragePathKind, string]) =>
+  path.join('.');
+
+export const keyToPath = (key: string) => {
+  const parts = key.split('.');
+  return [parts[0], parts.slice(1).join('.')] as [
+    AgoricChainStoragePathKind,
+    string,
+  ];
+};
+
+const parseIfJSON = (d: string | unknown) => {
+  if (typeof d !== 'string') return d;
+  try {
+    return JSON.parse(d);
+  } catch {
+    return d;
+  }
+};
+
+// Exported for testing.
+export const axiosClient = axios.create();
+
+axiosRetry(axiosClient, {
+  retries: 2,
+  retryDelay: axiosRetry.exponentialDelay,
+});
+
+export const vstorageQuery = async <T>(
+  node: string,
+  unmarshal: FromCapData<string>,
+  [pathKind, path]: [AgoricChainStoragePathKind, string],
+): Promise<{ blockHeight?: string; value: T }> => {
+  const url = new URL(`${node}/agoric/vstorage/${pathKind}/${path}`).href;
+  const request = axiosClient.get(url);
+
+  return request.then(({ data: res }) => {
+    if (pathKind === AgoricChainStoragePathKind.Children) {
+      return { value: res.children, blockHeight: undefined };
+    }
+
+    if (!res.value) {
+      return {
+        value: res.value,
+      };
+    }
+
+    const data = parseIfJSON(res.value);
+
+    const latestValue =
+      typeof data.values === 'undefined'
+        ? parseIfJSON(data.value)
+        : parseIfJSON(data.values[data.values.length - 1]);
+
+    const unserialized =
+      typeof latestValue.slots === 'undefined'
+        ? latestValue
+        : unmarshal(latestValue);
+
+    return {
+      blockHeight: data.blockHeight,
+      value: unserialized,
+    };
+  });
+};
--- packages/rpc/test/chainStorageWatcher.test.ts
+++ packages/rpc/test/chainStorageWatcher.test.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable import/extensions */
 import { expect, it, describe, beforeEach, vi, afterEach } from 'vitest';
+import MockAdapter from 'axios-mock-adapter';
 import { makeAgoricChainStorageWatcher } from '../src/chainStorageWatcher';
+import { axiosClient } from '../src/vstorageQuery';
 import { AgoricChainStoragePathKind } from '../src/types';
 
-const fetch = vi.fn();
-global.fetch = fetch;
 global.harden = val => val;
 
 const fakeApiAddr = 'https://fake.api';
@@ -14,24 +14,23 @@ const marshal = (val: unknown) => val;
 const unmarshal = (val: unknown) => val;
 
 let watcher: ReturnType<typeof makeAgoricChainStorageWatcher>;
+let mockAxios: MockAdapter;
+let onError: ReturnType<typeof vi.fn>;
 
 vi.mock('../src/marshal', () => ({ makeMarshal: () => {} }));
 
 describe('makeAgoricChainStorageWatcher', () => {
   beforeEach(() => {
-    watcher = makeAgoricChainStorageWatcher(
-      fakeApiAddr,
-      fakeChainId,
-      undefined,
-      {
-        fromCapData: unmarshal,
-        // @ts-expect-error mock doesnt require capdata type
-        toCapData: marshal,
-        unserialize: unmarshal,
-        // @ts-expect-error mock doesnt require capdata type
-        serialize: marshal,
-      },
-    );
+    onError = vi.fn();
+    mockAxios = new MockAdapter(axiosClient);
+    watcher = makeAgoricChainStorageWatcher(fakeApiAddr, fakeChainId, onError, {
+      fromCapData: unmarshal,
+      // @ts-expect-error mock doesnt require capdata type
+      toCapData: marshal,
+      unserialize: unmarshal,
+      // @ts-expect-error mock doesnt require capdata type
+      serialize: marshal,
+    });
     vi.useFakeTimers();
   });
 
@@ -47,15 +46,16 @@ describe('makeAgoricChainStorageWatcher', () => {
     const requestUrl1 = `${fakeApiAddr}/agoric/vstorage/data/${path}`;
     const requestUrl2 = `${fakeApiAddr}/agoric/vstorage/children/${path}`;
 
-    fetch.mockImplementation(requestUrl => {
-      if (requestUrl === requestUrl1) {
-        return createFetchResponse(AgoricChainStoragePathKind.Data, expected1);
-      }
-      return createFetchResponse(
-        AgoricChainStoragePathKind.Children,
-        expected2,
+    mockAxios
+      .onGet(requestUrl1)
+      .replyOnce(() =>
+        createFetchResponse(AgoricChainStoragePathKind.Data, expected1),
       );
-    });
+    mockAxios
+      .onGet(requestUrl2)
+      .replyOnce(() =>
+        createFetchResponse(AgoricChainStoragePathKind.Children, expected2),
+      );
 
     const value1 = new Promise(res => {
       watcher.watchLatest<string>(
@@ -74,43 +74,39 @@ describe('makeAgoricChainStorageWatcher', () => {
 
     expect(await value1).toEqual(expected1);
     expect(await value2).toEqual(expected2);
-
-    expect(fetch).toHaveBeenCalledTimes(2);
-    expect(fetch).toHaveBeenCalledWith(requestUrl1);
-    expect(fetch).toHaveBeenCalledWith(requestUrl2);
+    expect(mockAxios.history.get.length).toBe(2);
   });
 
   it('can do single queries', async () => {
     const expected = 126560000000;
     const path = 'test.fakePath';
 
-    fetch.mockImplementation(_ => {
-      return createFetchResponse(
-        AgoricChainStoragePathKind.Data,
-        JSON.stringify(expected),
-        undefined,
-        false,
+    mockAxios
+      .onGet()
+      .replyOnce(() =>
+        createFetchResponse(
+          AgoricChainStoragePathKind.Data,
+          JSON.stringify(expected),
+          undefined,
+          false,
+        ),
       );
-    });
 
-    const value = watcher.queryOnce<string>([
-      AgoricChainStoragePathKind.Data,
-      path,
-    ]);
+    const value = watcher.queryOnce([AgoricChainStoragePathKind.Data, path]);
 
     vi.advanceTimersToNextTimer();
     expect(await value).toEqual(expected);
-    expect(fetch).toHaveBeenCalledOnce();
+    expect(mockAxios.history.get.length).toBe(1);
 
     vi.advanceTimersToNextTimer();
-    expect(fetch).toHaveBeenCalledOnce();
+    expect(mockAxios.history.get.length).toBe(1);
   });
 
   it('notifies for changed data values', async () => {
     const expected1 = 'test result';
     const path = 'test.fakePath';
 
-    fetch.mockImplementation(_ => {
+    mockAxios.onGet().replyOnce(_ => {
       return createFetchResponse(
         AgoricChainStoragePathKind.Data,
         expected1,
@@ -131,10 +127,10 @@ describe('makeAgoricChainStorageWatcher', () => {
     );
     vi.advanceTimersToNextTimer();
     expect(await values[0].value).toEqual(expected1);
-    expect(fetch).toHaveBeenCalledOnce();
+    expect(mockAxios.history.get.length).toBe(1);
 
     const expected2 = `${expected1}foo`;
-    fetch.mockImplementation(_ => {
+    mockAxios.onGet().replyOnce(_ => {
       return createFetchResponse(
         AgoricChainStoragePathKind.Data,
         expected2,
@@ -144,14 +140,14 @@ describe('makeAgoricChainStorageWatcher', () => {
 
     vi.advanceTimersToNextTimer();
     expect(await values[1].value).toEqual(expected2);
-    expect(fetch).toHaveBeenCalledTimes(2);
+    expect(mockAxios.history.get.length).toBe(2);
   });
 
   it('notifies for changed children values', async () => {
     const expected1 = ['child1', 'child2'];
     const path = 'test.fakePath';
 
-    fetch.mockImplementation(_ => {
+    mockAxios.onGet().replyOnce(() => {
       return createFetchResponse(
         AgoricChainStoragePathKind.Children,
         expected1,
@@ -171,10 +167,10 @@ describe('makeAgoricChainStorageWatcher', () => {
     );
     vi.advanceTimersToNextTimer();
     expect(await values[0].value).toEqual(expected1);
-    expect(fetch).toHaveBeenCalledOnce();
+    expect(mockAxios.history.get.length).toBe(1);
 
     const expected2 = [...expected1, 'child3'];
-    fetch.mockImplementation(_ => {
+    mockAxios.onGet().replyOnce(() => {
       return createFetchResponse(
         AgoricChainStoragePathKind.Children,
         expected2,
@@ -183,14 +179,14 @@ describe('makeAgoricChainStorageWatcher', () => {
 
     vi.advanceTimersToNextTimer();
     expect(await values[1].value).toEqual(expected2);
-    expect(fetch).toHaveBeenCalledTimes(2);
+    expect(mockAxios.history.get.length).toBe(2);
   });
 
   it('can unsubscribe from paths', async () => {
     const expected1 = ['child1', 'child2'];
     const path = 'test.fakePath';
 
-    fetch.mockImplementation(_ => {
+    mockAxios.onGet().replyOnce(_ => {
       return createFetchResponse(
         AgoricChainStoragePathKind.Children,
         expected1,
@@ -210,12 +206,141 @@ describe('makeAgoricChainStorageWatcher', () => {
     );
     vi.advanceTimersToNextTimer();
     expect(await values[0].value).toEqual(expected1);
-    expect(fetch).toHaveBeenCalledOnce();
+    expect(mockAxios.history.get.length).toBe(1);
 
     unsub();
 
     vi.advanceTimersToNextTimer();
-    expect(fetch).toHaveBeenCalledOnce();
+    expect(mockAxios.history.get.length).toBe(1);
+  });
+
+  it('queryOnce retries up to two times on server errors', async () => {
+    const expected = 126560000000;
+    const path = 'test.fakePath';
+
+    const failedResponse = createFetchResponse(
+      AgoricChainStoragePathKind.Data,
+      '{}',
+      undefined,
+      false,
+      500,
+    );
+    mockAxios
+      .onGet()
+      .replyOnce(() => failedResponse)
+      .onGet()
+      .replyOnce(() => failedResponse)
+      .onGet()
+      .replyOnce(() =>
+        createFetchResponse(
+          AgoricChainStoragePathKind.Data,
+          JSON.stringify(expected),
+          undefined,
+          false,
+          200,
+        ),
+      );
+
+    const value = watcher.queryOnce([AgoricChainStoragePathKind.Data, path]);
+
+    vi.advanceTimersByTimeAsync(1000);
+    expect(await value).toEqual(expected);
+    expect(mockAxios.history.get.length).toBe(3);
+  });
+
+  it('queryOnce throws on three consecutive server errors', async () => {
+    const expected = 126560000000;
+    const path = 'test.fakePath';
+
+    const failedResponse = createFetchResponse(
+      AgoricChainStoragePathKind.Data,
+      '{}',
+      undefined,
+      false,
+      500,
+    );
+    mockAxios
+      .onGet()
+      .replyOnce(() => failedResponse)
+      .onGet()
+      .replyOnce(() => failedResponse)
+      .onGet()
+      .replyOnce(() => failedResponse);
+
+    const value = watcher.queryOnce([AgoricChainStoragePathKind.Data, path]);
+    vi.advanceTimersByTimeAsync(1000);
+
+    await expect(async () => value).rejects.toThrow(
+      'Request failed with status code 500',
+    );
+    expect(mockAxios.history.get.length).toBe(3);
+  });
+
+  it('watchLatest retries up to two times on server errors', async () => {
+    const expected1 = 'test result';
+    const path = 'test.fakePath';
+
+    const failedResponse = createFetchResponse(
+      AgoricChainStoragePathKind.Data,
+      '{}',
+      undefined,
+      true,
+      500,
+    );
+    mockAxios
+      .onGet()
+      .replyOnce(() => failedResponse)
+      .onGet()
+      .replyOnce(() => failedResponse)
+      .onGet()
+      .replyOnce(() =>
+        createFetchResponse(AgoricChainStoragePathKind.Data, expected1),
+      );
+
+    const result = new Promise(res => {
+      watcher.watchLatest<string>(
+        [AgoricChainStoragePathKind.Data, path],
+        value => res(value),
+      );
+    });
+    vi.advanceTimersByTimeAsync(1000);
+    expect(await result).toEqual(expected1);
+    expect(mockAxios.history.get.length).toBe(3);
+  });
+
+  it('watchLatest calls onError after three consecutive server errors', async () => {
+    const expected1 = 'test result';
+    const path = 'test.fakePath';
+
+    const failedResponse = createFetchResponse(
+      AgoricChainStoragePathKind.Data,
+      '{}',
+      undefined,
+      true,
+      500,
+    );
+    mockAxios
+      .onGet()
+      .replyOnce(() => failedResponse)
+      .onGet()
+      .replyOnce(() => failedResponse)
+      .onGet()
+      .replyOnce(() => failedResponse);
+
+    const result = new Promise((res, rej) => {
+      watcher.watchLatest<string>(
+        [AgoricChainStoragePathKind.Data, path],
+        value => res(value),
+      );
+      setTimeout(() => rej(new Error('timeout')), 1000);
+    });
+    vi.advanceTimersByTimeAsync(1000);
+
+    await expect(async () => result).rejects.toThrow('timeout');
+    expect(onError).toHaveBeenCalledWith(
+      new Error('Request failed with status code 500'),
+    );
+    expect(mockAxios.history.get.length).toBe(3);
   });
 });
 
@@ -224,22 +349,22 @@ const createFetchResponse = (
   value: unknown,
   blockHeight?: number,
   json = true,
-) => ({
-  json: () =>
-    new Promise(res =>
-      res(
-        kind === AgoricChainStoragePathKind.Children
-          ? { children: value }
-          : {
-              value: JSON.stringify({
-                values: json ? [JSON.stringify(marshal(value))] : undefined,
-                value: !json ? value : undefined,
-                blockHeight: String(blockHeight ?? 0),
-              }),
+  code = 200,
+) =>
+  new Promise<unknown[]>(res => {
+    res([
+      code,
+      kind === AgoricChainStoragePathKind.Children
+        ? { children: value }
+        : {
+            value: {
+              values: json ? [JSON.stringify(marshal(value))] : undefined,
+              value: !json ? value : undefined,
+              blockHeight: String(blockHeight ?? 0),
             },
-      ),
-    ),
-});
+          },
+    ]);
+  });
 
 const future = <T>() => {
   let resolve: (value: T) => void;
