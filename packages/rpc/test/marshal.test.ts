19f585df3907cb609ee9ef5c1eac6139f689e940
Dan Connolly <dckc@madmode.com>
2023-12-29 21:48:14 -0600

    test: inclue boardIDs in string version of synthetic remotables
     - explicit (primitive) String conversion
     - note issue for making @endo/init unnecessary
    
    Co-authored-by: Turadg Aleahmad <turadg@agoric.com>

---
+++ packages/rpc/test/marshal.test.ts
@@ -0,0 +1,10 @@
+import '@endo/init'; // UNTIL https://github.com/endojs/endo/issues/1686
+import { expect, test } from 'vitest';
+import { makeClientMarshaller } from '../src/marshal.js';
+
+test('inclue boardIDs in string version of synthetic remotables', () => {
+  const m = makeClientMarshaller();
+  const capData = { body: '#"$0.Brand"', slots: ['board0123'] };
+  const str = String(m.fromCapData(capData));
+  expect(str).toBe('[object Alleged: Brand#board0123]');
+});
