b45258de4d66df6aa5856d8165ab79200c7112f6
Turadg Aleahmad <turadg@agoric.com>
2024-04-17 09:32:52 -0700

    chore: yarn format

--- packages/react-components/vite.config.ts
+++ packages/react-components/vite.config.ts
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
-import { defineConfig } from 'vitest/config'
-import dts from 'vite-plugin-dts'
-import tailwindcss from 'tailwindcss'
-import { UserConfigExport } from 'vite'
-import { name } from './package.json'
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
+import dts from 'vite-plugin-dts';
+import tailwindcss from 'tailwindcss';
+import { UserConfigExport } from 'vite';
+import { name } from './package.json';
 
 const app = async (): Promise<UserConfigExport> => {
   /**
@@ -12,7 +12,7 @@ const app = async (): Promise<UserConfigExport> => {
    * @octocat/library-repo -> library-repo
    * vite-component-library-template -> vite-component-library-template
    */
-  const formattedName = name.match(/[^/]+$/)?.[0] ?? name
+  const formattedName = name.match(/[^/]+$/)?.[0] ?? name;
 
   return defineConfig({
     plugins: [
@@ -31,7 +31,7 @@ const app = async (): Promise<UserConfigExport> => {
         entry: path.resolve(__dirname, 'src/lib/index.ts'),
         name: formattedName,
         formats: ['es', 'umd'],
-        fileName: (format) => `${formattedName}.${format}.js`,
+        fileName: format => `${formattedName}.${format}.js`,
       },
       rollupOptions: {
         external: ['react', 'react/jsx-runtime', 'react-dom', 'tailwindcss'],
@@ -49,7 +49,7 @@ const app = async (): Promise<UserConfigExport> => {
       globals: true,
       environment: 'jsdom',
     },
-  })
-}
+  });
+};
 // https://vitejs.dev/config/
-export default app
+export default app;
